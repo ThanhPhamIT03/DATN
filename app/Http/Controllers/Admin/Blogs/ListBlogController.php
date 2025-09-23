527c4f444aaa2502380367262ad1f46bb2483d32
Phạm Văn Thành <thanhphamit.03@gmail.com>
2025-09-24 01:31:53 +0700

    created list blog

--- app/Http/Controllers/Admin/Blogs/ListBlogController.php
+++ app/Http/Controllers/Admin/Blogs/ListBlogController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Blogs;
 
 // Core
 use Illuminate\Routing\Controller;
+use Illuminate\Http\Request;
 
 // Helpers
 use Illuminate\Support\Facades\Auth;
@@ -11,15 +12,47 @@ use Illuminate\Support\Facades\Auth;
 // Models
 use App\Models\Blog\Blog;
 use App\Models\Blog\BlogContainer;
+use Illuminate\Support\Facades\Storage;
 
 class ListBlogController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $user = Auth::user();
 
+        $query = Blog::orderBy('created_at', 'desc');
+
+        if ($request->filled('keyword')) {
+            $query->where('title', 'like', '%' . $request->keyword . '%');
+        }
+
+        $blogs = $query->paginate(10)->withQueryString();
+
         return view('admin.pages.blogs.list-blog', [
-            'user' => $user
+            'user' => $user,
+            'blogs' => $blogs
         ]);
+    }
+
+    public function delete(Request $request)
+    {
+        $blog = Blog::find($request->id);
+        if (!$blog) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Bài viết không tồn tại!'
+            ], 404);
+        }
+
+        if ($blog->imthumbnailage && Storage::disk('public')->exists($blog->thumbnail)) {
+            Storage::disk('public')->delete($blog->thumbnail);
+        }
+
+        $blog->delete();
+
+        return response()->json([
+            'success' => true,
+            'message' => 'Xoá bài viết thành công!'
+        ], 200);
     }
 }
--- routes/admin/blog/list-blog.php
+++ routes/admin/blog/list-blog.php
@@ -6,4 +6,5 @@ use App\Http\Controllers\Admin\Blogs\ListBlogController;
 Route::controller(ListBlogController::class)->name('blog.list.')->prefix('blog/list')
     ->group(function() {
         Route::get('', 'index')->name('index');
+        Route::delete('delete', 'delete')->name('delete');
     });
